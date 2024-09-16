a81330621916aa0537037053fdbf9bd83246eba7
email ignatiousamalraj20@gmail.com <ignatiousamalraj20@gmail.com>
2024-09-16 21:55:49 +0530

    program added

---
+++ src/Testing/LearningGithub.java
@@ -0,0 +1,11 @@
+package Testing;
+
+public class LearningGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome github");
+
+	}
+
+}
